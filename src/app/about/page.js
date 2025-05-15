4d905a8118af6f15e1cec217f96c3e673d106231
Pantelis Karabetsos <panteliskarabetsos@Mac.home>
2025-05-15 21:25:23 +0300

    about page updates

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -100,7 +100,7 @@ export default function About() {
     </h1>
     <p className="text-base sm:text-lg md:text-xl leading-relaxed text-muted-foreground">
     Hi, I’m Pantelis Karabetsos. I’m studying Informatics and Computer Engineering at the University of West Attica and working my way into the field of software development. 
-    I enjoy building web applications that are clean, functional, and easy to use.
+    I enjoy building web applications that are clean, functional and easy to use.
     Whether I’m working on a full-stack feature, designing simple UI components or improving performance, 
     I try to focus on what makes the user experience better. 
     I’m always looking to learn, improve and build things that are useful and reliable.
